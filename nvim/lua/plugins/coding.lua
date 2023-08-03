fdbb508f6028c5c76aa3844fc35a17936eda4d78
Quoc Thi <quocthi0606@gmail.com>
2023-08-03 16:08:57 +0700

    lazy lazy lazy...

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { { "hrsh7th/cmp-cmdline" }, { "onsails/lspkind-nvim" } },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.cmdline = {
+        [":"] = {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = "path" },
+          }, {
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { "Man", "!" },
+              },
+            },
+          }),
+        },
+        ["/"] = {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = "buffer" },
+          },
+        },
+      }
+
+      local border_opts = {
+        border = "rounded",
+        winhighlight = "Normal:None,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+      }
+      opts.window = {
+        completion = cmp.config.window.bordered(border_opts),
+        documentation = cmp.config.window.bordered(border_opts),
+      }
+      opts.formatting = {
+        format = function(_, item)
+          item.menu = nil
+          item.kind = (require("lazyvim.config").icons.kinds[item.kind] or "") .. item.kind
+          return item
+        end,
+      }
+      opts.preselect = cmp.PreselectMode.None
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+      })
+      opts.mapping = {
+        ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c", "s" }),
+        ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c", "s" }),
+        -- ["<C-n>"] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_next_item()
+        --   elseif not cmp.complete() then
+        --     fallback()
+        --   end
+        -- end, { "i", "s", "c" }),
+        ["<C-g>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-y>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          select = false,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }
+    end,
+  },
+}
